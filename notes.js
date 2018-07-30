4ea4ff6cc6358b0888c82de719647173421841e1
Lindsey Bowen <lindsbow102@yahoo.com>
2018-07-30 16:48:37 -0700

    Add note function

--- notes.js
+++ notes.js
@@ -1,7 +1,30 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 const addNote = (title, body) => {
-    console.log(`Adding note: ${title}, ${body}`);
+    let notes = [];
+    const note = {
+        title,
+        body
+    };
+
+    // Load file
+    try {
+        const notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {
+
+    }
+
+    // To save file, first check if title is a duplicate or not
+    const duplicateNotes = notes.filter((note) => note.title === title); // if true, item will be kept in array
+
+    // Save file
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));  // Data must be string to write to file
+    }    
 };
 
 const getAll = () => {
