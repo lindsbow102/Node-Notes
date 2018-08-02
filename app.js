5db5fe76b6ab84ab9b0925ee30ebafc7080a447b
Lindsey Bowen <lindsbow102@yahoo.com>
2018-08-02 13:47:53 -0700

    List Notes wired up

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log("Starting app.js");
-
 const fs = require("fs");
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -22,7 +20,9 @@ if (command === 'add') {
     }  
 
 } else if (command === 'list') {
-    notes.getAll();
+    const allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s)!`);
+    allNotes.forEach(note => notes.logNote(note));
 
 } else if  (command === 'read') {
     const note = notes.getNote(argv.title);
--- notes.js
+++ notes.js
@@ -1,5 +1,3 @@
-console.log('Starting notes.js');
-
 const fs = require('fs');
 
 // Load file
@@ -34,7 +32,7 @@ const addNote = (title, body) => {
 };
 
 const getAll = () => {
-    console.log(`Getting all notes`);
+    return fetchNotes();
 };
 
 const getNote = (title) => {
